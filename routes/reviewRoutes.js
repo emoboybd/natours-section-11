4f77e11857fe7ad1f2eb8484606b1cefa6e20ca7
emoboybd <shahedurrahman.shakil53@gmail.com>
2023-04-05 19:42:06 +0600

    initial commit

---
+++ routes/reviewRoutes.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const reviewController = require('./../controllers/reviewController');
+const authController = require('./../controllers/authController');
+
+const router = express.Router({ mergeParams: true });
+
+// GET /tour/21313/reviews/3434535
+
+router
+  .route('/')
+  .get(reviewController.getAllReviews)
+  .post(
+    authController.protect,
+    authController.restrictTo('user'),
+    reviewController.setTourUserIds,
+    reviewController.createReview
+  );
+
+router
+  .route('/:id')
+  .delete(reviewController.deleteReview)
+  .patch(reviewController.updateReview);
+
+module.exports = router;
